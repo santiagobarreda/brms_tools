b19f2cb3b4097ade10c8fb194b8f2064b2ee4f9d
Santiago Barreda <sbarreda@ucdavis.edu>
2020-12-26 06:23:08 -0800

    .

--- data/functions.R
+++ data/functions.R
@@ -2,7 +2,7 @@
 ## function to make summary plots for coefficients
 
 brmplot = function (mat, ylim=NULL, xlim = NULL, horizontal = TRUE, add = FALSE, xs = NULL, col = 1, 
-                    labels = "",xlab='',ylab='', pch=16, lwd=2,cex=1.5, ...){
+                    labels = NULL,xlab='',ylab='', pch=16, lwd=2,cex=1.5, ...){
   
   n = nrow (mat)
   if (horizontal){
@@ -12,7 +12,7 @@ brmplot = function (mat, ylim=NULL, xlim = NULL, horizontal = TRUE, add = FALSE,
     if (is.null(labels)) labels = rownames(mat)  
     if (!add) plot (xs,mat[,1], col=col,pch=pch,cex=cex, ylim = ylim,xlim=xlim, xlab=xlab,xaxt='n',ylab = ylab,...)
     if (add) points (xs, mat[,1], col=col, pch=pch,cex=cex)
-    if (labels[1] !='') axis (side=1, at = 1:n, labels = labels, las=2)
+    if (!is.null(labels)) axis (side=1, at = 1:n, labels = labels, las=2)
     
     if (length(col)==1) col = rep (col, length(xs))
     for (i in 1:n) segments (xs[i],mat[i,3],xs[i],mat[i,4],lwd=lwd, col=col[i])
@@ -24,7 +24,7 @@ brmplot = function (mat, ylim=NULL, xlim = NULL, horizontal = TRUE, add = FALSE,
     if (is.null(ylim)) ylim = range (1:n)
     if (!add) plot (mat[,1],xs, pch=pch, col=col,cex=cex, ylim = ylim,xlim=xlim, ylab=ylab,yaxt='n',xlab = ylab,...)
     if (add) points (mat[,1],xs, pch=pch,cex=cex, col=col)
-    if (labels[1] !='') axis (side=2, at = n:1, labels = labels, las=1)
+    if (!is.null(labels)) axis (side=2, at = n:1, labels = labels, las=1)
     
     if (length(col)==1) col = rep (col, length(xs))
     for (i in n:1) segments (mat[i,3],xs[i],mat[i,4],xs[i],lwd=lwd, col=col[i])
